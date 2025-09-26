8b32d9cc0d6c706f0b2126f125ce6145ce4cc4d6
A1-20\A1-20 <>
2025-09-26 18:32:00 +0900

    0926학원 commit

---
+++ src/map/test/test06/Goods.java
@@ -0,0 +1,37 @@
+package map.test.test06;
+
+public class Goods {
+
+    private String name;
+    private int price;
+    private String status;
+
+    public Goods(String name, int price, String status){
+        this.name = name;
+        this.price=price;
+        this.status=status;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+
+    @Override
+    public String toString(){
+        return "상품명 : " + name + "상품가격 : " + price + "판매상태 : " + status;
+    }
+
+
+
+
+}
